e8d6ab0a062cdd42fa3ba28c6acd251060198bef
Aldric De Gryse <aldricdg@gmail.com>
2022-01-13 17:40:21 +0100

    starting on the offers division

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,26 @@
 class OffersController < ApplicationController
+  def index
+    @offers = Offer.all
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    @offer.save
+    redirect_to offer_category_path
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
+  end
+
+  private
+
+  def offer_params
+    params.require(:offer).permit(:price, :name, :offer_category, :category, :start_date, :end_date, :localisation)
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def offer_category
+    @offers = Offer.all
+  end
 end
